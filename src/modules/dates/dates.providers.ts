d160582f80a2ab335c7dd047a443043cf0a8814d
guido morabito <guidomorabito161@hotmail.com>
2026-04-09 00:10:25 -0300

    fixes 2

--- src/modules/dates/dates.providers.ts
+++ src/modules/dates/dates.providers.ts
@@ -1,7 +1,7 @@
 import { Provider } from '@nestjs/common';
 import { DATES_SHEET_PORT, DATES_TEMPORAL_SHEET_PORT } from './dates.tokens';
-import { GoogleSheetsDatesSheetAdapter } from './infraestructure/google-sheets-dates-sheet.adapter';
-import { GoogleSheetsTemporalAdapter } from './infraestructure/google-sheets-temporal.adapter';
+import { GoogleSheetsDatesSheetAdapter } from './adapters/google-sheets-dates-sheet.adapter';
+import { GoogleSheetsTemporalAdapter } from './adapters/google-sheets-temporal.adapter';
 
 export const datesProviders: Provider[] = [
   GoogleSheetsDatesSheetAdapter,
